b07d2f6368b80b2bd1679df9e6710c192da4fcc1
Wiktoria Banach <115671088+Wikuska@users.noreply.github.com>
2025-12-13 15:29:00 +0100

    Add area calculation feature

--- Geometry Game/Rectangle.py
+++ Geometry Game/Rectangle.py
@@ -4,3 +4,8 @@ class Rectangle:
         self.low_left = low_left
         self.top_right = top_right
         
+    def is_area_correct(self, area_guess):
+        if area_guess == (self.top_right.x - self.low_left.x) * (self.top_right.y - self.low_left.y):
+            return True
+        else:
+            return False
--- Geometry Game/main.py
+++ Geometry Game/main.py
@@ -10,3 +10,7 @@ print(f"Rectangle Coordinates: {rectangle.low_left.x},{rectangle.low_left.y} and
 user_point = Point(float(input("Guess X: ")), float(input("Guess Y: ")))
 
 print(f"Your answer is: {user_point.falls_in_rectangle(rectangle)}")
+
+user_area_guess = input("What is the area of this rectangle?: ")
+
+print(f"Your answer is: {rectangle.is_area_correct(user_area_guess)}")
